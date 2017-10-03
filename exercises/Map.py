48f0379584c5f22af89a337162b5d11fa28d03ca
Fares Fraij <fares@cs.utexas.edu>
2017-10-03 09:00:25 -0500

    upload additional material

--- exercises/Map.py
+++ exercises/Map.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python3
 
-#pylint: disable = consider-using-enumerate, too-few-public-methods
-
 # ------
 # Map.py
 # ------
-
-class Map_Iterator :
-    def __init__ (self, uf, a) :
-        self.uf = uf
-        self.p  = iter(a)
-
-    def __iter__ (self) :
-        return self
-
-    def __next__ (self) :
-        return self.uf(next(self.p))
 
 def map_range_for (uf, a) :
     for i in range(len(a)) :
@@ -24,6 +11,11 @@ def map_range_for (uf, a) :
 def map_for (uf, a) :
     for v in a :
         yield uf(v)
+
+def map_while (uf, a) :
+    p = iter(a)
+    while True :
+        yield uf(next(p))
 
 def map_generator (uf, a) :
     return (uf(v) for v in a)
